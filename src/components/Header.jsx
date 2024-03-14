c24f8e22afdbe657d3dc0f2b00cc338fc31d0f03
camillebagnani <140850865+camillebagnani@users.noreply.github.com>
2024-03-14 13:41:32 -0600

    set up React Vite app

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import Navigation from "./Navigation";
+
+function Header() {
+    return (
+        <div>
+            < Navigation />
+            <h1>Camille Bagnani</h1>
+        </div>
+    )
+}
+
+export default Header;
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,12 @@
+function Navigation() {
+    return (
+        <ul className="navbar">
+            <li>About Me</li>
+            <li>Portfolio</li>
+            <li>Contact</li>
+            <li>Resume</li>
+        </ul>
+    )
+}
+
+export default Navigation;
